6ba850b9bd3481f15950d4c55fa9b54ac768d3f1
Alf <sh4mil.nasyrov@gmail.com>
2024-06-23 13:54:08 +0500

    fin creating courier tests

--- tests/test_create_courier.py
+++ tests/test_create_courier.py
@@ -5,7 +5,7 @@ from data import TestData
 
 class TestCourier():
     @allure.title('Проверка возможности создания курьера')
-    @allure.description('Формируются post запрос со случайными строками в 10 символов')
+    @allure.description('При отправке корректного запроса создания курьера возвращается код 201')
     def test_creating_courier_return_201(self):
         login = Courier.generate_random_string(10)
         password = Courier.generate_random_string(10)
@@ -17,3 +17,56 @@ class TestCourier():
         }
         response = requests.post(TestData.courier_create_url, data=payload)
         assert response.status_code == 201
+
+    @allure.title('Проверка возвращаемого значения при корректном создании курьера')
+    @allure.description('При отправке корректного запроса создания курьера возвращается ok со значением true')
+    def test_creating_courier_return_true(self):
+        login = Courier.generate_random_string(10)
+        password = Courier.generate_random_string(10)
+        first_name = Courier.generate_random_string(10)
+        payload = {
+            "login": login,
+            "password": password,
+            "firstName": first_name
+        }
+        response = requests.post(TestData.courier_create_url, data=payload)
+        assert response.json()["ok"]
+
+    @allure.title('Проверка невозможности создания одинаковых курьеров')
+    @allure.description('Отправляем дважды одинаковый запрос')
+    def test_creating_courier_twice_return_409(self):
+        login = Courier.generate_random_string(10)
+        password = Courier.generate_random_string(10)
+        first_name = Courier.generate_random_string(10)
+        payload = {
+            "login": login,
+            "password": password,
+            "firstName": first_name
+        }
+        requests.post(TestData.courier_create_url, data=payload)
+        final_response = requests.post(TestData.courier_create_url, data=payload)
+        assert final_response.status_code == 409
+
+    @allure.title('Проверка отсутствия обязательного поля login при создании курьера')
+    @allure.description('Отправляем запрос без поля login')
+    def test_creating_courier_without_nessesary_login_field_return_400(self):
+        password = Courier.generate_random_string(10)
+        first_name = Courier.generate_random_string(10)
+        payload = {
+            "password": password,
+            "firstName": first_name
+        }
+        response = requests.post(TestData.courier_create_url, data=payload)
+        assert response.status_code == 400
+
+    @allure.title('Проверка отсутствия обязательного поля password при создании курьера')
+    @allure.description('Отправляем запрос без поля password')
+    def test_creating_courier_without_nessesary_password_field_return_400(self):
+        login = Courier.generate_random_string(10)
+        first_name = Courier.generate_random_string(10)
+        payload = {
+            "login": login,
+            "firstName": first_name
+        }
+        response = requests.post(TestData.courier_create_url, data=payload)
+        assert response.status_code == 400
